a30288f8070835ed9d13432e0b34384071267435
Alex <156783171+Algaram@users.noreply.github.com>
2025-06-12 22:45:35 -0700

    Create User.java
    add user entity with jpa annotations

---
+++ src/main/java/com/iam/iam_backend/User.java
@@ -0,0 +1,50 @@
+package com.iam.iambackend;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany
+    private Set<Role> roles = new HashSet<>();
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
+
+// tells springboot to make a user table in the database with an id, username, password, and roles
