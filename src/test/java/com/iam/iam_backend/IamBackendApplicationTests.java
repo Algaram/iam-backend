4336f86f832e7ceeda3b317c2a360a01c46f1222
Alex <156783171+Algaram@users.noreply.github.com>
2025-06-12 22:32:40 -0700

    Initial commit: Spring Boot IAM Project setup complete, commit on main branch

---
+++ src/test/java/com/iam/iam_backend/IamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iam.iam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
